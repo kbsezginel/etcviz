e28bc589933c1831f2c9afbc2e9b13caa24ca66f
kbsezginel <kbsezginel@othot.com>
2020-02-27 12:29:35 -0500

    Initialize setup and package structure

---
+++ etcviz/__init__.py
@@ -0,0 +1,4 @@
+"""
+Critter & Guitari ETC video synthesizer mode development and visualization tools.
+"""
+from .etc import ETC
---
+++ setup.py
@@ -0,0 +1,20 @@
+"""
+Setup file for ETC Video Synthesizer Mode Development Python package.
+"""
+from setuptools import setup, find_packages
+
+
+setup(
+    name="etcviz",
+    version="0.1.0",
+    description="Tools for ETC video synthesizer mode development",
+    author="Kutay B. Sezginel",
+    author_email="kutaybs@gmail.com",
+    url='https://github.com/kbsezginel/etcviz',
+    include_package_data=True,
+    packages=find_packages(),
+    install_requires=['pygame'],
+    entry_points={
+        'console_scripts': ['etcviz=etcviz.cli:main']
+    }
+)
